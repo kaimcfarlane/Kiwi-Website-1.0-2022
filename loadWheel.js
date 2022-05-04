70964294cf72cf92617f82b13453a25030dc432f
kaimcfarlane <kaimcfarlane43@gmail.com>
2022-05-04 10:52:29 -0400

    Added Load in Animation
    Added a animation that loads in homepage after 3500ms and sets the load wheel display to zero after 4000ms. Animation still needs some work but basics are done.

--- loadWheel.js
+++ loadWheel.js
@@ -1,13 +1,15 @@
 let loadWheel = document.querySelector(".loadWheel")
 let contentPage = document.querySelector(".contentPage")
-
+let span = document.querySelector("span")
 
 window.onload=()=>
 {
     setInterval(wheelAnimation, 4000)
+
 }
 
 function wheelAnimation() {
     loadWheel.style.display='none'; 
     contentPage.style.opacity = "1"; 
+    // span.style.animation = "1s ease-out 0s 1 contentLoad"; 
 }
