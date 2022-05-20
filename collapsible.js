3f7bc76c8a31a7777c11b4da227b5e1b5a4ae748
kaimcfarlane <kaimcfarlane43@gmail.com>
2022-05-20 15:42:40 -0400

    Finished About Page Center
    Added all button animations and fixed the page going to top afterwards. All that needs work are maybe some scroll or on load animations.

--- collapsible.js
+++ collapsible.js
@@ -2,13 +2,14 @@
 var coll = document.getElementsByClassName("wrapperExpButton");
 var i;
 
-for (i = 0; i < 2; i++) {
+for (i = 0; i < 3; i++) {
     if(i == 0) {
         coll[i].addEventListener("click", function() {
             this.classList.toggle("active");
             var content = document.querySelector(".aboutintro");
             if (content.style.display === "block") {
-            content.style.display = "none";
+            // content.style.display = "none";
+            content.classList.add("active");
             } else {
             content.style.display = "block";
             }
@@ -19,13 +20,28 @@ for (i = 0; i < 2; i++) {
             this.classList.toggle("active");
             var content = document.querySelector(".aboutmiddle");
             if (content.style.display === "block") {
-            content.style.display = "none";
+            // content.style.display = "none";
+            content.classList.add("active");
+            } else {
+            content.style.display = "block";
+            }
+        });
+    }
+    else if (i == 2) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = document.querySelector(".aboutend");
+            if (content.style.display === "block") {
+            // content.style.display = "none";
+            content.classList.add("active");
             } else {
             content.style.display = "block";
             }
         });
     }
 }
+
+//So either when clicked we put a css active style where we put an animation to change opacity
 
 //Heres the issue. The above code can yes acess all buttons with the words wrapperExpButton
 //But because of the loop we can only access elements we put so only about intro will go in and out
