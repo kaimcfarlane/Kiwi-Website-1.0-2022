7b2dcf1e65529dd505cc0052b16c9b32dc4023b8
kaimcfarlane <kaimcfarlane43@gmail.com>
2022-05-24 21:56:33 -0400

    Large Screen Size Addtions
    Fixed images for large screen size as well as HomePage text.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-        let width = screen.width;
-        let height = screen.height;
-        console.log(width + " " + height);
-        if (width > 1450) {
-            alert("Kiwielgato.com is optimized for screens under 1500 pixels wide. We encourage you to make your window smaller for better visibility - Kai McFarlane (Web-Developer)")}
+        // let width = screen.width;
+        // let height = screen.height;
+        // console.log(width + " " + height);
+        // if (width > 1450) {
+        //     alert("Kiwielgato.com is optimized for screens under 1500 pixels wide. We encourage you to make your window smaller for better visibility - Kai McFarlane (Web-Developer)")}
