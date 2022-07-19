9feaf0d9e43f145e5d55644fc70cc9aebab83447
kaimcfarlane <kaimcfarlane43@gmail.com>
2022-07-19 11:53:37 -0400

    Fixed error with mobile change going into desktop screen size.
    Made mobile view main title styling not pass into bigger screen sizes.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-        // let width = screen.width;
-        // let height = screen.height;
-        // console.log(width + " " + height);
-        // if (width > 2250) {
-        //     alert("Kiwielgato.com is optimized for screens under 2250 pixels wide. We encourage you to make your window smaller for better visibility - Kai McFarlane (Web-Developer)")}
+var el = document.getElementById("el");
+var bannerText = document.getElementById("bannerText");
+if (screen.width > 450)
+{
+        el.remove();
+        bannerText.innerText = "KIWI-EL-GATO";
+}
